d6c5340fcc5d2e53083cc2748de25ac85e3cd471
Anna Nagy-Staron <93430634+kis-moho@users.noreply.github.com>
2021-10-31 19:22:11 +0100

    Add files via upload

---
+++ 4_following_links_in_HTML.py
@@ -0,0 +1,33 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL -')
+repeat =7
+position = 18
+
+#to repeat desired times
+for i in range(repeat):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    count = 0
+    for tag in tags:
+        count = count +1
+
+        #stopping at desired position
+        if count>position:
+            break
+        url = tag.get('href', None)
+        name = tag.contents[0]
+
+print(name)
